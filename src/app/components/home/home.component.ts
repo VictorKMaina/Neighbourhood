d481299ae29ee81c6b77e9e6bb87a223165de9f8
Jeff-Mwai <jeffmwai3@gmail.com>
2020-12-04 14:58:32 +0300

    created hood array

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Hood } from 'src/app/hood';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+    hoods:Hood[] = [
+    {user:Jeff, hood:'Juja', posted_at:'', content'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'},
+    {id:2,name:'Buy Cookies'},
+    {id:3,name:'Get new Phone Case'},
+    {id:4,name:'Get Dog Food'},
+    {id:5,name:'Solve math homework'},
+    {id:6,name:'Plot my world domination plan'},
+  ];
   constructor() { }
 
   ngOnInit(): void {
   }
 
 }
+
+
+
--- src/app/hood.ts
+++ src/app/hood.ts
@@ -1,2 +1,6 @@
 export class Hood {
+  user: number;
+  hood: string;
+  posted_at: Date;
+  content: string;
 }
