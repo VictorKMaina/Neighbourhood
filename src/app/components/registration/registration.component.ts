f09ec1d09467b9fe8835ba462f6bac5a33d5dc17
Jeff-Mwai <jeffmwai3@gmail.com>
2020-12-06 12:48:19 +0300

    created data response

--- src/app/components/registration/registration.component.ts
+++ src/app/components/registration/registration.component.ts
@@ -1,5 +1,7 @@
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 import { Component, OnInit } from '@angular/core';
 import { UserReg } from 'src/app/class/user-reg';
+import { RegistrationService } from 'src/app/services/registration.service';
 
 @Component({
   selector: 'app-registration',
@@ -15,12 +17,15 @@ export class RegistrationComponent implements OnInit {
   password: string;
   new_user: any;
 
-  constructor() { }
+  constructor(private service: RegistrationService) { }
 
   ngOnInit(): void {
   }
   OnSubmit() {
     this.new_user = new UserReg(this.username, this.first_name, this.last_name, this.email, this.password);
-    console.log(this.new_user);
+    this.service.sendForm(this.new_user).subscribe((response : any) => {
+    console.log('Data', response)
+    this.new_user=response.data;
+    })
   }
 }
