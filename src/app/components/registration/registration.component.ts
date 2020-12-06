4cb1f91e01e6565b7d04dd97fc983cef834a35c9
Jeff-Mwai <jeffmwai3@gmail.com>
2020-12-06 12:29:31 +0300

    Added send user method

--- src/app/components/registration/registration.component.ts
+++ src/app/components/registration/registration.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserReg } from 'src/app/class/user-reg';
 
 @Component({
   selector: 'app-registration',
@@ -7,11 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationComponent implements OnInit {
 
+  username: string;
+  first_name: string;
+  last_name: string;
+  email: string;
+  password: string;
+  new_user: any;
+
   constructor() { }
 
   ngOnInit(): void {
   }
   OnSubmit() {
-  alert("submitted")
+    this.new_user = new UserReg(this.username, this.first_name, this.last_name, this.email, this.password);
+    console.log(this.new_user);
   }
 }
